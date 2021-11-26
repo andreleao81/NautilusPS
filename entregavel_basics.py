1a0ba22ec0d841281f1ef770569494e11900b6d0
andreleao81 <94919094+andreleao81@users.noreply.github.com>
2021-11-26 09:07:26 -0300

    Changed question 3

--- entregavel_basics.py
+++ entregavel_basics.py
@@ -1,49 +1,53 @@
-# Lista de Entregaveis Python Básico
-# André Leão, Engenharia Eletrônica e de Computação
+# Python Basics list for Nautilus
 
-# Questão 1
+# Question 1
 
-def pa_or_pg(ratio):
+def as_or_gs(ratio):
     num = 2
     maximum = 50
     result = list()
 
     while num <= maximum:
         result += [num]
+        # if ratio is even, add it to number (making an arithmetic sequence)
+        # if not, multiply number by it (making an geometric sequence) 
         num = num + ratio if ratio % 2 == 0 else num * ratio
     return result
 
-print(pa_or_pg(2))
 
 
-
-# Questao 2
+# Question 2
 
 def bitCounter(integer):
-    # non negative integer
-    # converte inteiro para o binario (str)
-    # '0b+binario'
+    # integer is non negative
+    # converts int to bin (str)
+    # 'binary' format is '0b+binario'
     binary = bin (integer) 
     result = sum([1 for i in binary if i == '1'])
     return result
 
 
 
-# Questao 3
+# Question 3
 
-def ways(amount = 200, maximum = 200):
-    coins = [1, 2, 5, 10, 20, 50, 100, 200]
-    return sum(ways(amount - coin, coin) for coin in coins if coin <= maximum if amount - coin >= 0)
+def ways(maximum = 200, coins = (1, 2, 5, 10, 20, 50, 100, 200)):
+    # compute all combinations
+    # iterates all the tuple
+    result = [1] + [0]*maximum
+    for coin in coins:
+        for i in range(coin, maximum+1):
+            result[i] += result[i-coin]
+    return result[maximum]
 
 
-# Questao 4
+# Question 4
 
 # series = 11 + 22 + 33 + ... + 1010 = 10405071317
 # last 10 digits of the series, 1**2 + 2**2 + 3**2 + ... + 1000**2
 
 def lastTen():
-    # soma elementos da lista cujos elementos
-    # sao todos os quadrados ate 1000
+    # sum elements from list
+    # list of square numbers from 1 to 1000
     soma = sum ([i**i for i in range(1,1001)])
     return str(soma)[-10:]
 
