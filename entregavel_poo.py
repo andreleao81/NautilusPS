e34e0b8daeb1b5ef74a52214b8833cbb806801c1
André Leão <94919094+andreleao81@users.noreply.github.com>
2021-11-29 14:41:52 -0300

    Added 4 items to the constructor

--- entregavel_poo.py
+++ entregavel_poo.py
@@ -3,49 +3,55 @@
 
 class Team:
 
-    def __init__(self, name, points):
+    def __init__(self, name, points, wins, losses, draws, goals_difference ):
         self.name = name 
-        self.score = points
+        self.points = points
+        self.w = wins
+        self.l = losses
+        self.d = draws
+        self.gd = goals_difference
 
     def __str__(self):
         # Informs team's name and score 
-        out = f"The Team {self.name} has scored {self.score} points at the league."
+        out = f"The Team {self.name} has scored {self.points} points at the league.\
+            \nCurrently, {self.name} has {self.w} wins, {self.l} defeats and {self.d} draws, with {self.gd} as it's goals difference."
         return out
 
     def __repr__(self):
-        # represents Teams with name and score
-        return f'{self.name}({self.score})'
+        # represents Teams with name and league points
+        return f'{self.name}({self.points})'
 
     def __add__(self, rival): 
-        # add scores 
-        return (self.score + rival.score)
+        # add points 
+        return (self.points + rival.points)
     
     def __sub__(self, rival):
-        # subtracts scores
-        return (self.score - rival.score)
+        # subtracts points
+        return (self.points - rival.points)
 
     def __gt__(self, rival):
         # compares if more than
-        return (self.score > rival.score)
+        return (self.points > rival.points)
     
     def __lt__(self, rival):
         # compares if less than
-        return (self.score < rival.score)
+        return (self.points < rival.points)
     
     def __eq__(self, rival):
         # compares if equal
-        return (self.score == rival.score)
+        return (self.points == rival.points)
     
     def __ne__(self, rival):
         # compares if not equal
-        return (self.score != rival.score)
+        return (self.points != rival.points)
     
     def __ge__(self, rival):
         # compares if more ou equal
-        return (self.score >= rival.score)
+        return (self.points >= rival.points)
     
     def __le__(self, rival):
         # compares if less or equal
-        return (self.score <= rival.score)
+        return (self.points <= rival.points)
 
-# format : team = Team (name, score
+# format : team = Team (name, points, wins, losses, draws, goals_difference)
+
